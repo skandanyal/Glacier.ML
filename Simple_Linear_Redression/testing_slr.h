f788acb9acbdefce210cadcb644b35e055bd899e
Skandan C Y <165555723+skandanyal@users.noreply.github.com>
2025-03-22 16:55:32 +0530

    Add files via upload

---
+++ Simple_Linear_Redression/testing_slr.h
@@ -0,0 +1,40 @@
+//
+// Created by Dell on 22-03-2025.
+//
+
+#ifndef TESTING_SLR_H
+#define TESTING_SLR_H
+
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <functional>
+
+
+class Simple_Linear_Regression {
+private:
+	float m, c;
+	std::vector<float> y;
+	std::vector<float> x;
+	int n;
+	float sum_x, sum_y;
+
+public:
+	Simple_Linear_Regression(std::vector<float> &x, std::vector<float> &y);
+	void train();
+	void test(std::vector<float> &x_test, std::vector<float> &y_test);
+	void print_values() const;
+	float predict(float a);
+	void predict_print(std::vector<float> x_predict);
+	std::vector<float> predict(std::vector<float> x_predict);
+
+private:
+	float calculate_m();
+	float calculate_c();
+};
+
+
+
+#endif //TESTING_SLR_H
