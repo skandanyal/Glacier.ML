023603d4870b4bbd9c72a2a6bf73e79abf82d4fb
skandanyal <skandanyalagach@gmail.com>
2026-08-23 21:31:18 +0530

    refactored: benchmark now performs warm-up runs, and aggregates emtrics over 5 runs by default

--- Benchmark_drivers/Classification_Models/LogisticRegressionDriver.cpp
+++ Benchmark_drivers/Classification_Models/LogisticRegressionDriver.cpp
@@ -4,170 +4,146 @@
 
 #include <iostream>
 #include <vector>
-#include "Models/LogisticRegression.hpp"
-#include "Utils/utilities.hpp"
+#include "Glacier/Models/LogisticRegression.hpp"
+#include "Glacier/Utils/utilities.hpp"
+#include <chrono>
+#include <numeric>
 
-int smtg() {
-
-  std::vector<std::vector<float>> x_train_500, x_train_1000, x_train_5000,
-      x_train_10000, x_train_50000, x_train_100000, x_train_140000, x_train,
-      x_test, x_val_cs, x_val;
-  std::vector<std::string> y_train_500, y_train_1000, y_train_5000,
-      y_train_10000, y_train_50000, y_train_100000, y_train_140000, y_train,
-      y_test, y_cal_cs, y_val;
-
-  Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-140000.csv",
-                           x_train_140000, y_train_140000);
-  Glacier::Models::Logistic_Regression iceberg_140000(x_train_140000, y_train_140000);
-
-
-  // hyperparameters
-  float alpha = 0.0001;
-  int iterations = 2000;
-
-
-  iceberg_140000.train(alpha, iterations);
-  // iceberg.print_Beta_values();
-  // Logistic_Regression iceberg(x_train, y_train);
-  // iceberg_test.print_predict(x_test, y_test);
-  //
-  // iceberg.train(alpha, iterations);
-  // iceberg.analyze(x_val, y_val);
-
-  return 0;
-}
+#define RUNS 5
 
 int main() {
     std::vector<std::vector<float>> x_train_500, x_train_1000, x_train_5000, x_train_10000, x_train_50000,
-    x_train_100000, x_train_140000, x_test;
+                                    x_train_100000, x_train_140000, x_test;
     std::vector<std::string> y_train_500, y_train_1000, y_train_5000, y_train_10000, y_train_50000,
-    y_train_100000, y_train_140000, y_test;
+                             y_train_100000, y_train_140000, y_test;
 
     // creating the dataset containers
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-500.csv", x_train_500, y_train_500, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-1000.csv", x_train_1000, y_train_1000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-5000.csv", x_train_5000, y_train_5000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-10000.csv", x_train_10000, y_train_10000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-50000.csv", x_train_50000, y_train_50000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-100000.csv", x_train_100000, y_train_100000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs-140000.csv", x_train_140000, y_train_140000, true);
-    Glacier::Utils::read_csv_c("../Datasets/cs_datasets/cs_val_2.csv", x_test, y_test, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-500.csv", x_train_500, y_train_500, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-1000.csv", x_train_1000, y_train_1000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-5000.csv", x_train_5000, y_train_5000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-10000.csv", x_train_10000, y_train_10000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-50000.csv", x_train_50000, y_train_50000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-100000.csv", x_train_100000, y_train_100000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs-140000.csv", x_train_140000, y_train_140000, true);
+    Glacier::Utils::read_csv_c("Datasets/cs_datasets/cs_val_2.csv", x_test, y_test, true);
 
     // hyperparameters - float alpha, int iterations
-    float alpha = 0.0001;
+    float alpha = 0.001;
     int iterations = 2000;
 
-    // training each model by measuring its time
+    // ---------------------- warm-up ----------------------
+    for (int i = 0; i < 5; i++) {
+        Glacier::Models::Logistic_Regression iceberg_500(x_train_500, y_train_500);
+        iceberg_500.train(alpha, iterations);
+        iceberg_500.predict(x_test, 0.5);
+    }
+
+    std::cout << "DATASET SIZE:\t\t TIME TAKEN\n";
+
     // 500 rows
-    auto start1 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_500(x_train_500, y_train_500);
-    iceberg_500.train(alpha, iterations);
-    iceberg_500.predict(x_test);
-
-    auto end1 = std::chrono::system_clock::now();
-    auto time1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
-
-    std::cout << "Time taken: " << time1.count() << " milli seconds \n";
-    // 4ms
+    std::vector<double> timing_500;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_500, y_train_500);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_500.push_back(elapsed.count());
+    }
+    double avg_500 = std::accumulate(timing_500.begin(), timing_500.end(), 0.0) / RUNS;
+    std::cout << "500\t\t " << avg_500 << " milli seconds \n";
 
     // 1000 rows
-    auto start2 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_1000(x_train_1000, y_train_1000);
-    iceberg_1000.train(alpha, iterations);
-    iceberg_1000.predict(x_test);
-
-    auto end2 = std::chrono::system_clock::now();
-    auto time2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2);
-
-    std::cout << "Time taken: " << time2.count() << " milli seconds \n";
-    // 6ms
+    std::vector<double> timing_1000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_1000, y_train_1000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_1000.push_back(elapsed.count());
+    }
+    double avg_1000 = std::accumulate(timing_1000.begin(), timing_1000.end(), 0.0) / RUNS;
+    std::cout << "1000\t\t " << avg_1000 << " milli seconds \n";
 
     // 5000 rows
-    auto start3 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_5000(x_train_5000, y_train_5000);
-    iceberg_5000.train(alpha, iterations);
-    iceberg_5000.predict(x_test);
-
-    auto end3 = std::chrono::system_clock::now();
-    auto time3 = std::chrono::duration_cast<std::chrono::milliseconds>(end3 - start3);
-
-    std::cout << "Time taken: " << time3.count() << " milli seconds \n";
-    // 25ms
+    std::vector<double> timing_5000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_5000, y_train_5000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_5000.push_back(elapsed.count());
+    }
+    double avg_5000 = std::accumulate(timing_5000.begin(), timing_5000.end(), 0.0) / RUNS;
+    std::cout << "5000\t\t " << avg_5000 << " milli seconds \n";
 
     // 10000 rows
-    auto start4 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_10000(x_train_10000, y_train_10000);
-    iceberg_10000.train(alpha, iterations);
-    iceberg_10000.predict(x_test);
-
-    auto end4 = std::chrono::system_clock::now();
-    auto time4 = std::chrono::duration_cast<std::chrono::milliseconds>(end4 - start4);
-
-    std::cout << "Time taken: " << time4.count() << " milli seconds \n";
-    // 50ms
+    std::vector<double> timing_10000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_10000, y_train_10000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_10000.push_back(elapsed.count());
+    }
+    double avg_10000 = std::accumulate(timing_10000.begin(), timing_10000.end(), 0.0) / RUNS;
+    std::cout << "10000\t\t " << avg_10000 << " milli seconds \n";
 
     // 50000 rows
-    auto start5 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_50000(x_train_50000, y_train_50000);
-    iceberg_50000.train(alpha, iterations);
-    iceberg_50000.predict(x_test);
-
-    auto end5 = std::chrono::system_clock::now();
-    auto time5 = std::chrono::duration_cast<std::chrono::milliseconds>(end5 - start5);
-
-    std::cout << "Time taken: " << time5.count() << " milli seconds \n";
-    // 248ms
+    std::vector<double> timing_50000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_50000, y_train_50000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_50000.push_back(elapsed.count());
+    }
+    double avg_50000 = std::accumulate(timing_50000.begin(), timing_50000.end(), 0.0) / RUNS;
+    std::cout << "50000\t\t " << avg_50000 << " milli seconds \n";
 
     // 100000 rows
-    auto start6 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_100000(x_train_100000, y_train_100000);
-    iceberg_100000.train(alpha, iterations);
-    iceberg_100000.predict(x_test);
-
-    auto end6 = std::chrono::system_clock::now();
-    auto time6 = std::chrono::duration_cast<std::chrono::milliseconds>(end6 - start6);
-
-    std::cout << "Time taken: " << time6.count() << " milli seconds \n";
-    // 491ms
+    std::vector<double> timing_100000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_100000, y_train_100000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_100000.push_back(elapsed.count());
+    }
+    double avg_100000 = std::accumulate(timing_100000.begin(), timing_100000.end(), 0.0) / RUNS;
+    std::cout << "100000\t\t " << avg_100000 << " milli seconds \n";
 
     // 140000 rows
-    auto start7 = std::chrono::system_clock::now();
-
-    Glacier::Models::Logistic_Regression iceberg_140000(x_train_140000, y_train_140000);
-    iceberg_140000.train(alpha, iterations);
-    iceberg_140000.predict(x_test);
-
-    auto end7 = std::chrono::system_clock::now();
-    auto time7 = std::chrono::duration_cast<std::chrono::milliseconds>(end7 - start7);
-
-    std::cout << "Time taken: " << time7.count() << " milli seconds \n";
-    // 695ms
-
-
-    // analyzing the output
-    iceberg_140000.analyze_2_targets(x_test, y_test);
-
-    /*
-    *[INFO]  Confusion matrix:
-    Actually was_in_trouble, Predicted was_in_trouble: 522
-    Actually was_in_trouble, Predicted was_not_in_trouble: 200
-    Actually was_not_in_trouble, Predicted was_in_trouble: 6362
-    Actually was_not_in_trouble, Predicted was_not_in_trouble: 2916
-    Total number of rows: 10000
-
-    [INFO]  Evaluation Metrics: (Out of 1)
-    Accuracy: 0.3438
-    Recall: 0.722992
-    False positive rate: 0.685708
-    Precision: 0.075828
-    */
+    std::vector<double> timing_140000;
+    for (int i = 0; i < RUNS; i++) {
+        auto start = std::chrono::system_clock::now();
+        Glacier::Models::Logistic_Regression model(x_train_140000, y_train_140000);
+        model.train(alpha, iterations);
+        std::vector<std::string> pred =
+            model.predict(x_test, 0.5);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        timing_140000.push_back(elapsed.count());
+    }
+    double avg_140000 = std::accumulate(timing_140000.begin(), timing_140000.end(), 0.0) / RUNS;
+    std::cout << "140000\t\t " << avg_140000 << " milli seconds \n";
 
     return 0;
-
 }
